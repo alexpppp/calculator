1539250af1eddec96bed0e41e3e842037884e0f1
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-03 18:47:32 +1200

    add base functionality, still equals function to do

--- calculator.js
+++ calculator.js
@@ -1,33 +1,103 @@
-var equation = []
+var entries = []
 var runningTotal = 0;
 var temp = ''
 
+document.body.addEventListener("click", calculate)
+//TODO only click event on button element
 
-// function equationPush to capture inputs in memory
+function calculate(e) {
+    
+    // get input
+    var val = e.target.textContent
+        // can return nodeName, href, id, className 
 
+     // if number, add to temp
+    if (!isNaN(val) || val ==='.') {
+        temp+= val
+    } else {
+    // if any symbol other than equals, add temp var to entries, then clear temp, then add current symbol to entries
+        entries.push(temp)
+        temp = '';
+        switch (val) {
+            // if AC, clear entries array
+            case "AC":
+                console.log("AC")
+                entries = [];
+                runningTotal = 0;
+                break;
+            // if CE, clear last entry (temp var)    
+            case "CE":
+                console.log("CE");
+                temp = '';
+                break;
+            case "%":
+                console.log("%")
+                entries.push('/100')
+                break;
+            case "÷":
+                console.log("÷")
+                entries.push('/')
+                break;
+            case "X":
+                console.log("X")
+                entries.push('*')
+                break;
+            case "-":
+                console.log("-")
+                entries.push(val)
+                break;
+            case "+":
+                console.log("+")
+                entries.push(val)
+                break;
+            case "=":
+                total()
+                break;
+            default:
+                console.log("Error: Your switch statement ran out of options so you hit the default!")
+                break;
+            }
 
-// function to switch cases based on number or symbol
+        function total() {
+            console.log("someone pushed =")
+            nt = Number(entries[0])
+            for (i in entries) {
+                var nextNum = Number(entries[i+1])
+                var symbol = entries[i];
+                
+                if (symbol === '+') { nt += nextNum; } 
+                else if (symbol === '-') { nt -= nextNum; } 
+                else if (symbol === '*') { nt *= nextNum; } 
+                else if (symbol === '/') { nt /= nextNum; }
+      
+                i++;
+            }
+            display(nt)
+        }
 
-// function calculate, takes equation array and updates runningTotal
-    // or action like clear
+        function display(i) {
+            console.log("display updated")
+            document.getElementById("display").setAttribute("placeholder", i)
+        }
+
+        
+    }
     
 
 
-// function to clear
-    // clears equation
-
-// function displayRunningTotal
-    // pushes running total into the display
-
-
+document.getElementById("display").setAttribute("placeholder", temp)
+console.log(entries)
+    
+// end of calculate()
+}
 
 
-// input
-// if number (from id display), add to temp var
-// if any symbol other than equals
-    //add temp var to equation, then add current symbol, then clear teamp
 
-// if CE, clear last entry (temp var)
+
+
+
+
+
 
 //switch
     // X to *
@@ -53,4 +123,6 @@ var temp = ''
     // put a '-' on the front of string
 
 
-// push number back to display
+    
+
+// push variable back to display
