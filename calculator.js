b477ce4af93047cb8808a52c3dacb113629e6734
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-03 16:14:39 +1200

    initial psuedo-coding, layout, and style

---
+++ calculator.js
@@ -0,0 +1,56 @@
+var equation = []
+var runningTotal = 0;
+var temp = ''
+
+
+// function equationPush to capture inputs in memory
+
+
+// function to switch cases based on number or symbol
+
+// function calculate, takes equation array and updates runningTotal
+    // or action like clear
+    
+
+
+// function to clear
+    // clears equation
+
+// function displayRunningTotal
+    // pushes running total into the display
+
+
+
+
+// input
+// if number (from id display), add to temp var
+// if any symbol other than equals
+    //add temp var to equation, then add current symbol, then clear teamp
+
+// if CE, clear last entry (temp var)
+
+//switch
+    // X to *
+    // divide symbol becomes /
+    // =, push temp to entries, perform calculate
+        
+
+    // var nt = Number(entries[0]);
+    // for (var i = 1; i < entries.length; i++) {
+    //   var nextNum = Number(entries[i+1])
+    //   var symbol = entries[i];
+      
+    //   if (symbol === '+') { nt += nextNum; } 
+    //   else if (symbol === '-') { nt -= nextNum; } 
+    //   else if (symbol === '*') { nt *= nextNum; } 
+    //   else if (symbol === '/') { nt /= nextNum; }
+      
+    //   i++;
+    // }
+
+
+// if result < 0
+    // put a '-' on the front of string
+
+
+// push number back to display
